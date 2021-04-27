44221d5bf6af474de18b00cf3a0f05bbda5b1a1b
Cnidarian <cmphillips23@gmail.com>
2021-04-26 21:44:44 -0500

    added validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -48,11 +48,11 @@ const pizzaController = {
 
     // update pizza by id
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id!' });
-                return;
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
                 }
                 res.json(dbPizzaData);
             })
@@ -64,8 +64,8 @@ const pizzaController = {
         Pizza.findOneAndDelete({ _id: params.id })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id!' });
-                return;
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
                 }
                 res.json(dbPizzaData);
             })
